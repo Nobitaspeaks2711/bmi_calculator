592797e881f76790b709ecabc9f2db77822b5fd8
Nobitaspeaks2711 <vaibhavbauti99@gmail.com>
2024-07-11 10:18:44 +0530

    BMI calculator project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [height, setHeight] = useState(160);
+  const [weight, setWeight] = useState(70);
+
+
+  function onWeightChange(event) {
+    setWeight(event.target.value)
+
+  }
+  function onHeightChange(event) {
+    setHeight(event.target.value)
+
+  }
+  const output = useMemo(() => {
+    const calculateHeight = height / 100;
+
+    return (weight / (calculateHeight * calculateHeight)).toFixed(1)
+
+  }, [weight, height]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <h1>BMI Calculator
+        <h2 className='sub'>Body Mass Index</h2>
+      </h1>
+      <div className='input-section'>
+        <p className='slider-output'>Weight : {weight} kg  </p>
+        <input className='input-slider'
+          type='range'
+          step="1"
+          min="40"
+          max="150"
+          onChange={onWeightChange} />
 
-export default App;
+        <p className='slider-output'>Height :
+          {height}cm </p>
+        <input className='input-slider'
+          type="range"
+          onChange={onHeightChange}
+          min="100"
+          max="200" />
+      </div>
+      <div className='output-section'>
+        <p>Your BMI is</p>
+
+        <p className='output'>{output}</p>
+      </div>
+      <footer className='footer'>*Healthy BMI range:18.5 kg/m2 - 25kg/m2</footer>
+    </main>
+  );
+};
+
+export default App
